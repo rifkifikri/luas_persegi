6b4a82a051ded0c277988145c4e0f9387a66475c
rifkifikri <rifkifikri000@gmail.com>
2021-03-21 11:44:33 -0700

    luas dengan method

--- Luas.java
+++ Luas.java
@@ -2,19 +2,29 @@ package luas_persegi_panjang;
 import java.util.Scanner;
 
 public class Luas {
+	//class method
+	public int setLuas(int panjang, int lebar) {
+		int hasil;
+		hasil=panjang*lebar;
+		return hasil;
+	}
+	
 	public static void main(String[] args){
 		int panjang,luas,lebar;
 		Scanner userInput = new Scanner(System.in);//library yang di sediakan java untuk input data user
-
+		
+		Luas persegi = new Luas();//membuat object untuk memanggil
+		
 		//menghitung luas persegi panjang
 		System.out.print("masukkan nilai panjang=");
 		panjang=userInput.nextInt();//nextint untuk input yang bertipe data integer;
 		System.out.print("masukkan nilai lebar=");
 		lebar = userInput.nextInt();
 		
-		luas= panjang*lebar;
+		luas= persegi.setLuas(panjang, lebar);
 		
 		System.out.print("luasnya adalah = " + luas);
+		
 	}
 	
 }
